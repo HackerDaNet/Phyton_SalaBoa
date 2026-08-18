7bc36d0197945b9c58a5594df378d8d8f0b8db55
HackerDaNet <murilo_fodi@estudante.sesisenai.org.br>
2026-08-18 09:16:36 -0300

    commit interessante

---
+++ AtividadesBDePhyton/main.py
@@ -0,0 +1,55 @@
+import mysql.connector
+from config import DB_CONFIG
+from banco import criar_tabela
+import produtos
+from models import Produto
+import relatorio
+import banco
+
+# --- Uso ---
+def exibir_lista(lista):
+    if lista:
+        for produto in lista:
+            produto.exibir()
+    else:
+        print("Produto não encontrado!")
+def menu():
+    while True:
+        print("\n===== SISTEMA DE PRODUTOS =====")
+        print("1 - Cadastrar produto")
+        print("2 - Listar produtos")
+        print("3 - Buscar produto")
+        print("4 - Atualizar preço")
+        print("5 - Excluir produto")
+        print("0 - Sair")
+
+        opcao = input("Opção: ")
+
+        if opcao == "1":
+            nome = input("Nome: ")
+            preco = float(input("Preço: "))
+            qtd = int(input("Quantidade: "))
+            cat = input("Categoria: ")
+            novo_produto = Produto(nome, preco, qtd, cat)
+            produtos.cadastrar_produto(novo_produto)
+        elif opcao == "2":
+            exibir_lista(produtos.listar_produtos())
+        elif opcao == "3":
+            termo = input("Buscar por nome: ")
+            exibir_lista(produtos.buscar_produto(termo))
+        elif opcao == "4":
+            pid = int(input("ID do produto: "))
+            novo = float(input("Novo preço: "))
+            produtos.atualizar_preco(pid, novo)
+        elif opcao == "5":
+            pid = int(input("ID do produto: "))
+            produtos.excluir_produto(pid)
+        elif opcao == "0":
+            print("Encerrando...")
+            break
+        else:
+            print("Opção inválida!")
+
+#--- Chamado das funções ---
+
+menu()                   #executa de modo recorrente
